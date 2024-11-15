0f5916c0dde5eaf54e146b16cfbd608cf2e78cdf
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-11-15 17:40:09 -0500

    Input component and Basic Form

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,9 +1,26 @@
 import React from "react";
 
 import "./NewPlace.css";
+import Input from "../../shared/components/FormElements/Input";
 
 const NewPlace = () => {
-    return <h2>NewPlace Component!!</h2>;
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        id="title"
+        placeholder="Enter Title"
+        label="Title"
+      />
+      <Input
+        element="textarea"
+        id="description"
+        label="Title"
+        rows="5"
+      />
+    </form>
+  );
 };
 
 export default NewPlace;
